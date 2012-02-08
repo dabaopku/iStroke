9eb7b537d75581939b1d003e99111c8ef75934d5
dabao <dabao>
2012-02-09 01:31:53 +0800

    Use OSX Quartz Event Taps, very success!

--- iStroke/iStrokeAppDelegate.m
+++ iStroke/iStrokeAppDelegate.m
@@ -12,9 +12,29 @@
 
 @synthesize window;
 
+#import <ApplicationServices/ApplicationServices.h>
+
+CGEventRef myCGEventCallback(CGEventTapProxy proxy, CGEventType type,  CGEventRef event, void *refcon) {
+    printf("%u\n", (uint32_t)type);
+    return event; 
+}
+
+int hook () {
+    CFMachPortRef eventTap;  
+    CFRunLoopSourceRef runLoopSource; 
+    
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, kCGEventMaskForAllEvents, myCGEventCallback, NULL);
+    runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+    CGEventTapEnable(eventTap, true);
+    //CFRunLoopRun();
+    return 0;
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    hook();
 }
 
 @end
